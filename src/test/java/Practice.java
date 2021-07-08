53864d63e420f5efc9cad7d28644f3927755736b
Adem Cigdem <adem_cigdem10@hotmail.com>
2021-07-08 19:42:41 +0100

    adding Practice tests

---
+++ src/test/java/Practice.java
@@ -0,0 +1,71 @@
+public class Practice {
+    public static void main(String[] args) {
+
+//        System.out.println(test1());
+//        System.out.println(test2());
+//        System.out.println(test3());
+//        System.out.println(test4());
+    }
+
+
+
+
+    public static int test1(){
+        int a =0;
+        int b = 100;
+        int c = a * b +5%2;
+        return c;
+    }
+
+
+
+
+
+
+
+    public static int test2(){
+        return "Welcome to World".length();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public static String test3(){
+//        String [] arr = {"I","Like","You","Much"};
+//        String s ;
+//        for (int i = 0; i < arr.length; i++) {
+//            s = arr[i]+"";
+//        }
+//        return s;
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public static int test4(){
+//        int sum;
+//        int [] [] arr = {{1,4,8},{2,6,9}};
+//        sum+=arr[1][2];
+//        return sum;
+//    }
+
+
+}
