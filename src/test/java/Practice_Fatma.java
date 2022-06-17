cde6e2c0954d49ed20418a5e4c71b1f692ac2a74
fatmacekimguler <fatmacekimguler@gmail.com>
2022-06-17 21:27:43 +0100

    second commit

--- src/test/java/Practice_Fatma.java
+++ src/test/java/Practice_Fatma.java
@@ -1,5 +1,7 @@
 public class Practice_Fatma {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("My first GitHub Practice");
     }
 }
