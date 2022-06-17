b997a61f3c0a210cadd25a696d6dfb45bf193adf
OzturkSongul <103609835+OzturkSongul@users.noreply.github.com>
2022-06-17 21:00:25 +0100

    update Test.java

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -8,10 +8,7 @@ double sd=89.90;
    int i=5;
    int b=10; //from berkan
 
-      System.out.println("ihsan was here");
-      System.out.println("ihsan was here");
-      //from ihsan
-
+      System.out.println("b = " + b);
 
 
 
