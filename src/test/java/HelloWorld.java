6f7dd3acc5cec9fd1be1bcf87dc9b62cce91d09c
idrisDogruoz <111895940+idrisdogruoz@users.noreply.github.com>
2023-04-06 23:52:50 +0300

    2hello2

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,5 +1,15 @@
 public class HelloWorld {
     public static void main(String[] args) {
+
+
+
         System.out.println("Hello World");
+        System.out.println("Hello 1");
+        System.out.println("Hello 2");
+        System.out.println("Hello 3");
+        System.out.println("Hello 4");
+        System.out.println("Hello 5");
+
+
     }
 }
