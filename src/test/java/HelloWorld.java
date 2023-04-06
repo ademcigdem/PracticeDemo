9b8cb2826ab3469324005f71809b9dca9a743910
samur79 <112698551+samur79@users.noreply.github.com>
2023-04-06 21:49:12 +0100

    new conflict created

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -6,6 +6,9 @@ public class HelloWorld {
         System.out.println("Group-24 was here second time");
         System.out.println("Zubeyir was here second time");
         System.out.println("Zubeyir was here third time");
+        for (int i=0;i<=10;i++){
+            System.out.println("lets create a conflict");
+        }
 
     }
 }
