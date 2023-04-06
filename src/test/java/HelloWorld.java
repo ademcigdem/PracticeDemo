90da658b2c65836363170605c1769821a8b5300b
Adem <adem_cigdem10@hotmail.com>
2023-04-06 20:58:18 +0100

    update HelloWorld.java

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        System.out.println("Hello new World");
+        System.out.println("Group-24 was here");
+
     }
 }
