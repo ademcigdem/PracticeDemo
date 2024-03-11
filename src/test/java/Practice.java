0fe38c498e53aa7a68ae8321e39bcf840a8d5134
Adam Cigdem <60587675+ademcigdem@users.noreply.github.com>
2024-03-11 21:03:05 +0000

    Update Practice.java

--- src/test/java/Practice.java
+++ src/test/java/Practice.java
@@ -1,12 +1,7 @@
 public class Practice {
     public static void main(String[] args) {
 
-//        System.out.println(test1(5, 4));
-//        System.out.println(test1(4, 6, 8));
-//        System.out.println(lenght());
-//        System.out.println(test3());
-//        System.out.println(test4());
-//        System.out.println(prime(5));
+
         System.out.println(reverse("Samra"));
 
         // Roberto // From Sait
